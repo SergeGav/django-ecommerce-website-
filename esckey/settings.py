4b858de5490dcf938ede60bccb2e89a240372c35
Serge Gavrilov <sergegavrilov01@gmail.com>
2023-11-29 20:24:43 +0000

    review the code

--- esckey/settings.py
+++ esckey/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6x^e!nqgq8zox9$bfmq765%&o@#cqx+7^%mh+gouxgwye#**ul'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool) #True
 
 ALLOWED_HOSTS = []
 
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
     'store',
     'carts',
     'orders',
+    'admin_honeypot',
 
 
     'django.contrib.admin',
@@ -150,11 +152,8 @@ MESSAGE_TAGS = {
 }
 
 #SMTP configuration 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sergegavrilov01@gmail.com'
-DEFAULT_FROM_EMAIL = 'sergegavrilov01@gmail.com'
-SERVER_EMAIL = 'sergegavrilov01@gmail.com'
-EMAIL_HOST_PASSWORD = 'cdyv crjh hhll ksuc'
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
